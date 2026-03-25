00c9fc0a874716df3d8808deed4657d99dbce6da
VictorConrado <estudantecomv@gmail.com>
2026-03-25 00:09:40 -0300

    chore: Iniciando o projeto FlowDesk

---
+++ FlowDesk.API/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlowDesk.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("API rodando");
+    }
+}
